ee584973dad2729bf0a7ef1b2cf10be273a85102
nazar2009azAZ <nazikgavr@gmail.com>
2023-04-27 19:03:48 +0300

    PIDARS POFICKSY

--- chromeDriver/inst_spamer.py
+++ chromeDriver/inst_spamer.py
@@ -1,8 +1,11 @@
 from selenium import webdriver
+import time
 driver = webdriver.Chrome(executable_path=r"C:\bomber-pyTelegramBotApi\chromeDriver\chromedriver.exe")
 url = "https://www.instagram.com/"
 
 try:
+    driver.get(url=url)
+    time.sleep(5)
 except Exception as ex:
     print(ex)
 finally:
