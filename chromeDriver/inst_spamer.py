3752bdc0fbe0ddad514ab519e6a3857b63d290d4
nazar2009azAZ <nazikgavr@gmail.com>
2023-04-27 18:41:25 +0300

    a

--- chromeDriver/inst_spamer.py
+++ chromeDriver/inst_spamer.py
@@ -1,2 +1,17 @@
+from selenium import webdriver
+driver = webdriver.Chrome(executable_path=r"C:\bomber-pyTelegramBotApi\chromeDriver\chromedriver.exe")
+url = "https://www.instagram.com/"
+
+try:
+except Exception as ex:
+    print(ex)
+finally:
+    driver.close()
+    driver.quit()
+
+
 def startInstagram(link):
     pass
+
+
+
